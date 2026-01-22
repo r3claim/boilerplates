e25df6324babbf5d1d8e7d10a932b4275680aa7f
r3claim <31155111+r3claim@users.noreply.github.com>
2026-01-22 20:43:02 +0100

    Add root check and configure NTP servers for chrony
    Added root user check and updated NTP server configuration.

--- Linux/install-chrony.sh
+++ Linux/install-chrony.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
 
+if [ ! "$(id -u)" -eq 0 ]; then
+    echo -e "Fehler: Dieses Skript muss als root / mit sudo ausgeführt werden."
+    exit 1
+fi
+
 echo "Installiere chrony..."
 apt update
 apt install -y chrony
 
 echo "Konfiguriere NTP-Server..."
-cat >> /etc/chrony/chrony.conf << 'EOF'
+cat > /etc/chrony/conf.d/01-timeserver.conf << 'EOF'
 pool 0.de.pool.ntp.org iburst
 pool 1.de.pool.ntp.org iburst
 pool 2.de.pool.ntp.org iburst
